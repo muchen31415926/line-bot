61478a750590bb4c40c36faecb7fa14d7801b456
mu <muchen31415926@gmail.com>
2026-05-04 21:47:09 +0800

    wip: firebase storage setup (temporary implementation)

--- functions/api/line_file_assistant/services.js
+++ functions/api/line_file_assistant/services.js
@@ -1,34 +1,32 @@
-import { initializeApp, credential } from "firebase-admin/app";
+import { initializeApp, applicationDefault } from "firebase-admin/app";
 import { getStorage } from "firebase-admin/storage";
 import { createClient } from "@supabase/supabase-js";
 import { messagingApi } from "@line/bot-sdk";
 import { GoogleGenAI } from "@google/genai";
-
-const firebaseServiceAccount = JSON.parse(
-  process.env.FIREBASE_SERVICE_ACCOUNT_KEY,
-);
 
 export const lineConfig = {
   channelSecret: process.env.LINE_SECRET_ROB_V1,
   channelAccessToken: process.env.LINE_ACCESS_TOKEN_ROB_V1,
 };
 
-// Initialize firebase admin sdk (app of name: default)
-initializeApp({
-  credential: credential.cert(firebaseServiceAccount),
-});
+export const lineBlobClient = new messagingApi.MessagingApiBlobClient(
+  lineConfig,
+);
+
+export const lineClient = new messagingApi.MessagingApiClient(lineConfig);
+
+export const GenAI = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
 export const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_SECRET_KEY,
 );
 
+console.log("BUCKET:", process.env.FIREBASE_STORAGE_BUCKET);
+// Initialize firebase admin sdk (app of name: default)
+initializeApp({
+  credential: applicationDefault(),
+  storageBucket: "bucket reference",
+});
+
 export const bucket = getStorage().bucket();
-
-export const GenAI = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
-
-export const lineClient = new messagingApi.MessagingApiClient(lineConfig);
-
-export const lineBlobClient = new messagingApi.MessagingApiBlobClient(
-  lineConfig,
-);
